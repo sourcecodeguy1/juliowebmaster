df694db145b069edfe8b7ab700d3daf522d8c653
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2022-04-11 02:19:55 -0700

    changes to App.test file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("HELLO I'M JULIO. Full-stack Developer Home Resume Profile Details Matthew is a pianist living in Nashville.", { textContent: true });
   expect(linkElement).toBeInTheDocument();
 });
