84df9e25553c24a9b1da8fe8ce41d96e6c653728
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2022-04-12 00:32:58 -0700

    made changes to SemanticHeader.styled.js file

--- src/components/HeaderComponent/SemanticHeader.styled.js
+++ src/components/HeaderComponent/SemanticHeader.styled.js
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import { Segment, Container } from 'semantic-ui-react';
 
-export const StyledSegment = styled(Segment)({
-    backgroundColor: '#000080 !important',
-    color: 'white',
-    textAlign: 'center',
-    fontSize: '40px !important',
-    borderRadius: 'unset !important'
-});
+export const StyledSegment = styled(Segment)`
+    background-color: #000080 !important;
+    color: white;
+    text-align: center;
+    font-size: 40px !important;
+    border-radius: unset !important;
+    
+    @media screen and (max-width: 500px){
+        font-size: 30px !important;
+    }
+`;
 
 export const StyledContainer= styled(Container)({
     color: 'white',
