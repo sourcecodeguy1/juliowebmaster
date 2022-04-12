94f277ffe6a0b8cdaa605fbed197ee5ab460bdb1
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2022-04-11 19:20:53 -0700

    changes to App.test file

--- src/App.test.js
+++ src/App.test.js
@@ -4,17 +4,17 @@ import App from './App';
 
 
 test('renders learn react link', () => {
-  /*const {getByText} = render(<App />);
+  const {getByText} = render(<App />);
   expect(
       // - content: text content of current element, without text of its children
       // - element.textContent: content of current element plus its children
       screen.getByText((content, element) => {
         return content !== '' && element.textContent === 'HELLO! I\'M JULIO.';
       })
-  ).toBeInTheDocument();*/
+  ).toBeInTheDocument();
 
- const {getByText} = render(<App />);
-  const linkElement = screen.getByText(getByText);
-  expect(linkElement).toBeInTheDocument();
+ /* const {getByText} = render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();*/
 
 });
