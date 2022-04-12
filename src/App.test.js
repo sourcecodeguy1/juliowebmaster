9493a99b85f55e20923c52a9b401a672ae88d012
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2022-04-11 20:25:55 -0700

    changes to App.test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
-
 test('renders learn react link', () => {
-  /*const {getByText} = render(<App />);
-  expect(
-      // - content: text content of current element, without text of its children
-      // - element.textContent: content of current element plus its children
-      screen.getByText((content, element) => {
-        return content !== '' && element.textContent === 'HELLO! I\'M JULIO.';
-      })
-  ).toBeInTheDocument();*/
-
-  const {getByText} = render(<App />);
-  const linkElement = screen.getByText((content, element) => {
-    return content !== '' && element.textContent === 'HELLO! I\'M JULIO.';
-  });
-  expect(linkElement).toBeInTheDocument();
-
+ /* render(<App />);
+  const linkElement = screen.getByText(/HELLO I'M JULIO/i, {selector: 'div'});
+  expect(linkElement).toBeInTheDocument();*/
+  const getByTextContent = (text) => {
+    // Passing function to `getByText`
+    return screen.getByText((content, element) => {
+      const hasText = element => element.textContent === text
+      const elementHasText = hasText(element)
+      // eslint-disable-next-line testing-library/no-node-access
+      const childrenDontHaveText = Array.from(element?.children || []).every(child => !hasText(child))
+      return elementHasText && childrenDontHaveText
+    })
+  }
 });
