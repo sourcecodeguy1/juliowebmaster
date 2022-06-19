576af09af43cacd58c82110ee158fe76fb87f72a
sourcecodeguy1 <sandovaljulio2@yahoo.com>
2022-06-19 13:36:36 -0700

    update deploy.yml

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-//test('renders learn react link', () => {
+test('renders learn react link', () => {
  /* render(<App />);
   const linkElement = screen.getByText(/HELLO I'M JULIO/i, {selector: 'div'});
   expect(linkElement).toBeInTheDocument();*/
-  /*const getByTextContent = (text) => {
+  const getByTextContent = (text) => {
     // Passing function to `getByText`
     return screen.getByText((content, element) => {
       const hasText = element => element.textContent === text
@@ -14,5 +14,5 @@ import App from './App';
       const childrenDontHaveText = Array.from(element?.children || []).every(child => !hasText(child))
       return elementHasText && childrenDontHaveText
     })
-  }*/
-//});
+  }
+});
